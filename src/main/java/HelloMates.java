6a3579d1d5b613c590a4435d1242f39a6bef5154
Daria <68664175+dariapikul@users.noreply.github.com>
2020-07-23 20:12:20 +0300

    Training 4

--- src/main/java/HelloMates.java
+++ src/main/java/HelloMates.java
@@ -1,5 +1,6 @@
 public class HelloMates {
     public static void main(String[] args) {
-        System.out.println("Hello, Mates!");
+        System.out.print("Hello,");
+        System.out.println(" Mates!");
     }
 }
