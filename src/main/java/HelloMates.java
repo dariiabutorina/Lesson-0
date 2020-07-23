0164f8d0a4a24751a2c80f9b40785fd505f4a32e
Daria <68664175+DariaPikul@users.noreply.github.com>
2020-07-23 20:21:40 +0300

    Update HelloMates.java

--- src/main/java/HelloMates.java
+++ src/main/java/HelloMates.java
@@ -1,6 +1,5 @@
 public class HelloMates {
     public static void main(String[] args) {
-        System.out.print("Hello,");
-        System.out.println(" Mates!");
+        System.out.println("Hello, Mates!");
     }
 }
